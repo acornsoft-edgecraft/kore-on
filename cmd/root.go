dc3e977ee88f5ec852378260c005475444a2f8db
이동묵 <dongmook@acornsoft.io>
2022-12-22 17:13:28 +0900

    update dockerfile

--- cmd/root.go
+++ cmd/root.go
@@ -22,13 +22,6 @@ var RootCmd = &cobra.Command{
 	Long:  `cube, It install kubernetes cluster.`,
 }
 
-// RootCmd represents the base command when called without any subcommands
-var KoreOnCtlCmd = &cobra.Command{
-	Use:   "koreonctl",
-	Short: "Install kubernetes cluster to on-premise system with registry and storage system",
-	Long:  `cube, It install kubernetes cluster.`,
-}
-
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
@@ -51,8 +44,6 @@ func init() {
 		baremetal.AirGapCmd(),
 		baremetal.TestCmd(),
 	)
-	KoreOnCtlCmd.AddCommand()
-
 }
 
 func initConfig() {
