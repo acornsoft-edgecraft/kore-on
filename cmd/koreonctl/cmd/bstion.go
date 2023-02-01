36f3652d1448e49ab5f2a9b636e097a218d3094e
이동묵 <dongmook@acornsoft.io>
2023-02-01 20:39:59 +0900

    update bastion

--- cmd/koreonctl/cmd/bstion.go
+++ cmd/koreonctl/cmd/bstion.go
@@ -241,13 +241,14 @@ func (c *strBstionCmd) dockerInstall() error {
 				"./docker.keyring",
 			}
 			runExecCommand(commandArgs)
-			commandArgs = []string{
-				"sudo",
-				"apt-add-repository",
-				"deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable",
-			}
-			runExecCommand(commandArgs)
-			// exec.Command("bash", "-c", `sudo echo "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list`)
+			// commandArgs = []string{
+			// 	"sudo",
+			// 	"apt-add-repository",
+			// 	"deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable",
+			// }
+			// runExecCommand(commandArgs)
+			exec.Command(`sudo echo "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list`)
+			// exec.Command(`sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"`)
 
 			commandArgs = []string{
 				"sudo",
