d3f59ddbb110f6941382e849f13509c7dceb510c
이동묵 <dongmook@acornsoft.io>
2023-02-01 13:14:42 +0900

    update bastion for ubuntu

--- cmd/koreonctl/cmd/bstion.go
+++ cmd/koreonctl/cmd/bstion.go
@@ -222,12 +222,10 @@ func (c *strBstionCmd) dockerInstall() error {
 			}
 			runExecCommand(commandArgs)
 			commandArgs = []string{
-				"sudo",
 				"curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg",
 			}
 			runExecCommand(commandArgs)
 			commandArgs = []string{
-				"sudo",
 				"echo 'deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu " +
 					"$(lsb_release -cs) stable' | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
 			}
