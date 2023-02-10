abeab20323d714c1ed25748c257e0a1908502192
이동묵 <dongmook@acornsoft.io>
2023-02-10 14:25:24 +0900

    update cluster update

--- cmd/koreonctl/conf/templates/temp-cluster-get-kubeconfig.go
+++ cmd/koreonctl/conf/templates/temp-cluster-get-kubeconfig.go
@@ -1,14 +1,22 @@
 package templates
 
 const ClusterGetKubeconfigText = `
-{{- $Master := .KoreOnTemp.NodePool.Master}}
+{{- $Master := .KoreOnTemp.NodePool.Master }}
+{{- $private_ip := "" }}
+{{- $check_ip := false }}
+{{- if eq (len $Master.IP) (len $Master.PrivateIP) }}
+{{- $check_ip = true }}
+{{- end }}
 
 ## Inventory for {{.Command}} task.
 {{ printf "%.*s" 64 "======================================================================================================================================================" }}
 {{"Node Name"|printf "%-*s" 20}}{{"IP"|printf "%-*s" 22}}{{"Private IP"|printf "%-*s" 22}}
 {{ printf "%.*s" 64 "======================================================================================================================================================" }}
 {{-  range $index, $data := $Master.IP}}
-node-{{$index|printf "%-*v" 15}}{{$data|printf "%-*s" 22}}{{(index $Master.PrivateIP $index)|printf "%-*s" 22}}
+{{- if $check_ip }}
+{{- $private_ip = (index $Master.PrivateIP $index)}}
+{{- end }}
+node-{{$index|printf "%-*v" 15}}{{$data|printf "%-*s" 22}}{{$private_ip|printf "%-*s" 22}}
 {{- break }}
 {{-  end}}
 {{ printf "%.*s" 64 "======================================================================================================================================================" }}
